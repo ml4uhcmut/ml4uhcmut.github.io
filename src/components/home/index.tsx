0b3ce2590dc5cb31559e6d7a17ff6dfb5d23e723
Duc Dung Nguyen <saitamandd@users.noreply.github.com>
2025-04-04 14:45:55 +0700

    Update index.tsx

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -5,12 +5,15 @@ export default function HomePage() {
         <h1 className="xl:text-6xl font-bold lg:text-4xl text-2xl">
           ML4U - AITech Lab
         </h1>
-        <p>The AITech Lab at HCMUT is dedicated to driving AI research and development. With a focus on computer vision and speech processing, we provide a collaborative environment for faculty, students, and researchers to explore cutting-edge AI technologies.
-            We support shared infrastructure for research and encourage research that bridges different fields and leads to innovative solutions.
-            We connect our researchers with industry partners to explore real-world applications.
-            In addition, we host events, workshops, and discussions to foster knowledge sharing and collaboration.
-            By investing in promising AI areas and supporting our students' research endeavors, we aim to position HCMUT as a leading center for AI innovation.
-        </p>
+        <p>The AITech Lab at HCMUT is dedicated to driving AI research and development. With a focus on computer vision and speech processing, we provide a collaborative environment for faculty, students, and researchers to explore cutting-edge AI technologies.</p>
+<p><strong>Our lab aims for:</strong></p>
+<ul>
+<li><strong>Incubate ambitious AI projects:</strong>&nbsp;We support faculty-led AI initiatives and provide shared infrastructure for research.</li>
+<li><strong>Foster interdisciplinary collaboration:</strong>&nbsp;We encourage research that bridges different fields and leads to innovative solutions.</li>
+<li><strong>Engage with the industry:</strong>&nbsp;We connect our researchers with industry partners to explore real-world applications.</li>
+<li><strong>Nurture a vibrant research community:</strong>&nbsp;We host events, workshops, and discussions to foster knowledge sharing and collaboration.</li>
+</ul>
+<p><strong>By investing in promising AI areas and supporting our students&apos; research endeavors, we aim to position HCMUT as a leading center for AI innovation.</strong></p>
       </div>
     </div>
   );
